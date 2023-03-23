4dd19e53d89698b7fe0156c4581e6a4c71371074
silvi-hdez <silviahdez.b@gmail.com>
2023-03-23 15:30:59 +0100

    useMemo

--- src/contexts/ThemeContext.jsx
+++ src/contexts/ThemeContext.jsx
@@ -1,4 +1,5 @@
-import { createContext, useState } from "react"
+import { createContext, useCallback, useMemo, useState } from "react"
+import { useLocation } from "react-router-dom"
 
 const THEME_LOCAL_STORAGE_KEY = 'theme' 
 const supportedThemes = ['light', 'dark']
@@ -15,17 +16,23 @@ const getInitialTheme = () => {
 }
 
 export const ThemeProvider = ({children}) => {
+    // const location = useLocation()
+   
     const [theme, setTheme] = useState(getInitialTheme())
 
-    const toggleTheme = () => {
+    const toggleTheme = useCallback(() => {
         // setTheme(theme === 'light' ? 'dark' : 'light')
         const newThemeValue = theme === 'light' ? 'dark' : 'light'
         setTheme(newThemeValue)
         localStorage.setItem(THEME_LOCAL_STORAGE_KEY, newThemeValue)
-    }
+    }, [theme])
+
+    const value = useMemo(() => {
+        return {theme, toggleTheme}
+    }, [theme, toggleTheme])
 
     return(
-        <ThemeContext.Provider value={{theme:theme, toggleTheme:toggleTheme}}>
+        <ThemeContext.Provider value={value}>
             {children}
         </ThemeContext.Provider>
     )
