867d0356eb274fb7d44dd49116427f0f2433095a
silvi-hdez <silviahdez.b@gmail.com>
2023-03-23 15:22:26 +0100

    theme with localstorage

--- src/contexts/ThemeContext.jsx
+++ src/contexts/ThemeContext.jsx
@@ -1,19 +1,31 @@
 import { createContext, useState } from "react"
 
+const THEME_LOCAL_STORAGE_KEY = 'theme' 
 const supportedThemes = ['light', 'dark']
 
 const ThemeContext = createContext('light')
 export default ThemeContext
 
+const validateTheme = (themeValue) => {
+    return supportedThemes.includes(themeValue) && themeValue
+}
+
+const getInitialTheme = () => {
+    return validateTheme(localStorage.getItem(THEME_LOCAL_STORAGE_KEY) || 'light')
+}
+
 export const ThemeProvider = ({children}) => {
-    const [theme, setTheme] = useState('light')
+    const [theme, setTheme] = useState(getInitialTheme())
 
     const toggleTheme = () => {
-        setTheme(theme === 'light' ? 'dark' : 'light')
+        // setTheme(theme === 'light' ? 'dark' : 'light')
+        const newThemeValue = theme === 'light' ? 'dark' : 'light'
+        setTheme(newThemeValue)
+        localStorage.setItem(THEME_LOCAL_STORAGE_KEY, newThemeValue)
     }
 
     return(
-        <ThemeContext.Provider value={{theme: theme, toggleTheme: toggleTheme}}>
+        <ThemeContext.Provider value={{theme:theme, toggleTheme:toggleTheme}}>
             {children}
         </ThemeContext.Provider>
     )
